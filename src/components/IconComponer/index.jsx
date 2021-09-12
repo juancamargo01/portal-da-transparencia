7dcb841f9c577fa0f6446c21f8a9dbc8d4a250a7
Juan Camargo <juan.09.camargo@gmail.com>
2021-09-12 20:26:28 -0300

    refactore navebar

---
+++ src/components/IconComponer/index.jsx
@@ -0,0 +1,47 @@
+import {library} from '@fortawesome/fontawesome-svg-core';
+import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
+const IconComponer = (props) => {
+
+    const {icon , text, id, databstarget, link1, name1, link2, name2, link3 ,name3, link4, name4, link5, name5, link6, name6 } = props;
+
+
+    return (
+        <>
+            <div className="col-12 col-sm-4 text-center mb-5">
+                        <div className="d-grid">
+                            <button className="btn btn-white mb-3" type="button" data-bs-toggle="collapse" data-bs-target={databstarget}
+                                aria-expanded="false" aria-controls={id}>
+                                <h3>                                    
+                                    <div className="icon  text-primary mx-sm-3 mx-2">
+                                    <FontAwesomeIcon icon = {icon}/>
+                                    </div>
+                                    <p><strong>{text}</strong></p>
+                                </h3>
+                            </button>
+                        </div>
+                        <div className="collapse" id={id}>
+                            <div className="card card-body">
+                                <p><a href={link1}
+                                        target="_blank">{name1}</a></p>
+                                <p><a href={link2}
+                                        target="_blank">{name2}</a></p>
+                                <p><a href={link3}
+                                        target="_blank">{name3}</a></p>
+                                <p><a href={link4}
+                                        target="_blank">{name4}</a></p>
+                                <p><a href={link5}
+                                        target="_blank">{name5}</a></p>
+                                <p><a href={link6}
+                                        target="_blank">{name6}</a></p>
+                            </div>
+                        </div>
+                    </div>
+               </>        
+    );
+}
+
+export default IconComponer;
--- src/components/NaveBar/index.tsx
+++ src/components/NaveBar/index.tsx
@@ -1,168 +1,105 @@
 
-import {library} from '@fortawesome/fontawesome-svg-core';
-import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {fas} from '@fortawesome/free-solid-svg-icons';
+
+import IconComponer from 'components/IconComponer';
 
 
-library.add(fas);
+
 const NavBar = () => {
    
     return (
         <section className="py-5 bg-light" id="servicos">
 		<div className="container mt-sm-5">
-
-
-
 			<div className="row">
-				<div className="col-12 col-sm-4 text-center mb-5 d-grip grap-2">
-					<div className="d-grid">
-						<button className="btn btn-white mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#c1"
-							aria-expanded="false" aria-controls="c1" >
-							<h3>
-								<div className="icon  text-primary mx-sm-3 mx-2">
-                                <FontAwesomeIcon icon = "desktop"/>
-                                </div>
-								<p><strong>Gestão Contábil</strong></p>
-							</h3>
-						</button>
-					</div>
-					<div className="collapse" id="c1">
-						<div className="card card-body">
-							<p><a href="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/comparativo-receita-prevista-com-arrecadada/"
-									target="_blank">Receitas</a></p>
-							<p><a href="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-despesa-simplificado/"
-									target="_blank">Despesas</a></p>
-						</div>
-					</div>
-				</div>
 
-				<div className="col-12 col-sm-4 text-center mb-5">
-					<div className="d-grid">
-						<button className="btn btn-white mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#c2"
-							aria-expanded="false" aria-controls="c2">
-							<h3>
-								
-                                <div className="icon  text-primary mx-sm-3 mx-2">
-                                <FontAwesomeIcon icon = "check"/>
-                                </div>
-								<p><strong>Gestão Pessoal</strong></p>
-							</h3>
-						</button>
-					</div>
-					<div className="collapse" id="c2">
-						<div className="card card-body">
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/concursoseselecoes/"
-									target="_blank">Concursos e Seleções</a></p>
-							<p><a href="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-diaria-passagem/"
-									target="_blank">Diárias e Passagens</a></p>
-							<p><a href="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-folha-pagamento/"
-									target="_blank">Folha de Pagamento</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/vagas%20por%20cargo.pdf"
-									target="_blank">Relação de Vagas Legais por Cargos</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/gestaomunicipal/secretariaseorgaos/"
-									target="_blank">Secretarias e Órgãos</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/tabela%20salarial%20de%20cargos%20-%202017.pdf"
-									target="_blank">Tabela Salarial de Cargos</a></p>
-						</div>
-					</div>
-				</div>
+				<IconComponer icon="desktop"
+						 text="Gestão Contábil"
+						 databstarget="#c1"
+						 id="c1"						 
+						 link1="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/comparativo-receita-prevista-com-arrecadada/"
+						 name1="Receitas"
+						 link2="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-despesa-simplificado/"
+						 name2="Despesas"		
+				/>
 
-				<div className="col-12 col-sm-4 text-center mb-5">
-					<div className="d-grid">
-						<button className="btn btn-white mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#c3"
-							aria-expanded="false" aria-controls="c3">
-							<h3>								
-                                <div className="icon  text-primary mx-sm-3 mx-2">
-                                <FontAwesomeIcon icon = "clone"/>
-                                </div>
-								<p><strong>Gestão Orçamentária</strong></p>
-							</h3>
-						</button>
-					</div>
-					<div className="collapse" id="c3">
-						<div className="card card-body">
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/concursoseselecoes/"
-									target="_blank">Concursos e Seleções</a></p>
-							<p><a href="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-diaria-passagem/"
-									target="_blank">Diárias e Passagens</a></p>
-							<p><a href="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-folha-pagamento/"
-									target="_blank">Folha de Pagamento</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/vagas%20por%20cargo.pdf"
-									target="_blank">Relação de Vagas Legais por Cargos</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/gestaomunicipal/secretariaseorgaos/"
-									target="_blank">Secretarias e Órgãos</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/tabela%20salarial%20de%20cargos%20-%202017.pdf"
-									target="_blank">Tabela Salarial de Cargos</a></p>
-						</div>
-					</div>
-				</div>
+				<IconComponer icon="check"
+						 text="Gestão Pessoal"
+						 databstarget="#c2"
+						 id="c2"						 
+						 link1="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/concursoseselecoes/"
+						 name1="Concursos e Seleções"
+						 link2="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-diaria-passagem/"
+						 name2="Diárias e Passagens"
+						 link3="https://sigp.aparecida.go.gov.br/sig/app.html#/transparencia/transparencia-folha-pagamento/"
+						 name3="Folha de Pagamento"
+						 link4="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/vagas%20por%20cargo.pdf"
+						 name4="Relação de Vagas Legais por Cargos"	
+						 link5="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/gestaomunicipal/secretariaseorgaos/"
+						 name5="Secretarias e Órgãos"	
+						 link6="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/tabela%20salarial%20de%20cargos%20-%202017.pdf"
+						 name6="Tabela Salarial de Cargos"		
+				/>
 
-				<div className="col-12 col-sm-4 text-center mb-5">
-					<div className="d-grid">
-						<button className="btn btn-white mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#c4"
-							aria-expanded="false" aria-controls="c4">
-							<h3>
-                                <div className="icon  text-primary mx-sm-3 mx-2">
-                                    <FontAwesomeIcon icon = "check-double"/>
-                                </div>
-								<p><strong>Licitação e Contratos</strong></p>
-							</h3>
-						</button>
-					</div>
-					<div className="collapse" id="c4">
-						<div className="card card-body">
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/concursoseselecoes/"
-									target="_blank">Concursos e Seleções</a></p>
+				<IconComponer icon="check"
+						 text="Gestão Orçamentária"
+						 databstarget="#c3"
+						 id="c3"						 
+						 link1="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/gestaoorcamentaria/ppa/"
+						 name1="Plano PluriAnual"
+						 link2="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/gestaoorcamentaria/ldo/"
+						 name2="Leis e Diretrizes Orçamentárias"
+						 link3="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/gestaoorcamentaria/loa/"
+						 name3="Lei Orçamentária Anual"
+						 link4="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/gestaoorcamentaria/lrf/"
+						 name4="LRF - Relatórios"	
+						 link5="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/gestaoorcamentaria/balancos/"
+						 name5="Balanço"	
+						 	
+				/>
 
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/tabela%20salarial%20de%20cargos%20-%202017.pdf"
-									target="_blank">Tabela Salarial de Cargos</a></p>
-						</div>
-					</div>
-				</div>
+				<IconComponer icon="check-double"
+						 text="Licitação e Contratos"
+						 databstarget="#c4"
+						 id="c4"						 
+						 link1="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/concursoseselecoes/"
+						 name1="Concursos e Seleções"
+						 link2="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/tabela%20salarial%20de%20cargos%20-%202017.pdf"
+						 name2="Tabela Salarial de Cargos"		
+				/>
 
-				<div className="col-12 col-sm-4 text-center mb-5">
-					<div className="d-grid">
-						<button className="btn btn-white mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#c5"
-							aria-expanded="false" aria-controls="c5">
-							<h3>
-                                <div className="icon  text-primary mx-sm-3 mx-2">
-                                    <FontAwesomeIcon icon = "clipboard-list"/>
-                                </div>
-								<p><strong>Controle Social</strong></p>
-							</h3>
-						</button>
-					</div>
-					<div className="collapse" id="c5">
-						<div className="card card-body">
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/concursoseselecoes/"
-									target="_blank">Concursos e Seleções</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/tabela%20salarial%20de%20cargos%20-%202017.pdf"
-									target="_blank">Tabela Salarial de Cargos</a></p>
-						</div>
-					</div>
-				</div>
+				<IconComponer icon="clipboard-list"
+						 text="Controle Social"
+						 databstarget="#c5"
+						 id="c5"						 
+						 link1="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/controlesocial/cmt/"
+						 name1="Conselho Municipal de Transparência"
+						 link2="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/controlesocial/ceg/"
+						 name2="Conselho de Eficácia de Gestão"		
+				/>
 
-				<div className="col-12 col-sm-4 text-center mb-5">
-					<div className="d-grid">
-						<button className="btn btn-white mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#c6"
-							aria-expanded="false" aria-controls="c6">
-							<h3>
-                                <div className="icon  text-primary mx-sm-3 mx-2">
-                                    <FontAwesomeIcon icon = "desktop"/>
-                                </div>
-								<p><strong>Outras Consultas</strong></p>
-							</h3>
-						</button>
-					</div>
-					<div className="collapse" id="c6">
-						<div className="card card-body">
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/p/services/concursoseselecoes/"
-									target="_blank">Concursos e Seleções</a></p>
-							<p><a href="http://transparencia.aparecida.go.gov.br/portaltransparencia/assets/docs/tabela%20salarial%20de%20cargos%20-%202017.pdf"
-									target="_blank">Tabela Salarial de Cargos</a></p>
-						</div>
-					</div>
-				</div>
+				<IconComponer icon="plus-square"
+						 text="Outras Consultas"
+						 databstarget="#c6"
+						 id="c6"						 
+						 link1="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/cartilhasemanuais/"
+						 name1="Cartilhas e Manuais"
+						 link2="https://doe.aparecida.go.gov.br/"
+						 name2="Diário Oficial"	
+						 link3="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/participacaopopular/"
+						 name3="Participação Popular"
+						 link4="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/faq/"
+						 name4="Perguntas Frequentes"	
+						 link5="http://transparencia.aparecida.go.gov.br/portal-transparencia/p/services/funerarias/"
+						 name5="Rodízio de Funerárias"	
+				/>
+				
+
+				
+
+				
+
+				
+
+				
 
 			</div>
 			
